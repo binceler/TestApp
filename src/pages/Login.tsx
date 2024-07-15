9d0256cda563b8853acba39b6174ad6df0211ac3
Büşra Inceler <busrainceler@gmail.com>
2024-07-15 19:17:58 +0300

    creating login and register page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,22 +1,44 @@
-import { IonContent, IonFooter, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCard, IonCardContent, IonContent, IonFooter, IonHeader, IonIcon, IonInput, IonPage, IonTitle, IonToolbar, useIonRouter } from '@ionic/react';
 import React from 'react';
+import { logInOutline, personCircleOutline } from 'ionicons/icons'
+import FCC from '../assets/fcc.svg'
 
 const Login: React.FC = () => {
 
     const doLogin = (event : any) => {
+        const router = useIonRouter();
         event.preventDefault();
         console.log("doLogin");
+       // router.push('/home', 'root');
     }
 
     return (
         <IonPage>
             <IonHeader>
                 <IonToolbar color={'success'}>
-                    <IonTitle>Page Title</IonTitle>
+                    <IonTitle>Free Code Camp</IonTitle>
                 </IonToolbar>
             </IonHeader>
-            <IonContent>
-               Test
+            <IonContent scrollY={false}>
+                <div className='ion-text-center ion-padding'>
+                    <img src={ FCC } alt='FCC Logo' width={'50%'}></img>
+                </div>
+               <IonCard>
+                    <IonCardContent>
+                        <form onSubmit={doLogin}>
+                            <IonInput fill='outline' labelPlacement='floating' label="Email" type="email" placeholder="email@domain.com"></IonInput>
+                            <IonInput className='ion-margin-top' fill='outline' labelPlacement='floating' label='Password' type='password'></IonInput>
+                            <IonButton className='ion-margin-top' type='submit' color={'tertiary'} expand='block'>
+                                Login
+                                <IonIcon icon={ logInOutline } slot="end"></IonIcon>
+                                </IonButton>
+                            <IonButton className='ion-margin-top' routerLink='/register' type='button' expand='block'>
+                                Create Account
+                                <IonIcon icon={ personCircleOutline } slot="end"></IonIcon>
+                                </IonButton>
+                        </form>
+                    </IonCardContent>
+               </IonCard>
             </IonContent>
             
         </IonPage>
