9abdeec486cb3472ecc5bf0a4c9b736ac4a63dab
Büşra Inceler <busrainceler@gmail.com>
2024-09-22 15:57:55 +0300

    creating introduction slides and swiper

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,47 +1,61 @@
 import { IonButton, IonCard, IonCardContent, IonContent, IonFooter, IonHeader, IonIcon, IonInput, IonPage, IonTitle, IonToolbar, useIonRouter } from '@ionic/react';
-import React from 'react';
+import React, { useState } from 'react';
 import { logInOutline, personCircleOutline } from 'ionicons/icons'
 import FCC from '../assets/fcc.svg'
+import Intro from '../component/Intro';
 
 const Login: React.FC = () => {
+    const router = useIonRouter();
+    const [introSeen, setIntroSeen] = useState(false);
 
     const doLogin = (event : any) => {
-        const router = useIonRouter();
+        
         event.preventDefault();
         console.log("doLogin");
        // router.push('/home', 'root');
     }
 
+    const finishIntro = async () => {
+      console.log('FIN');
+      setIntroSeen(true);  
+    };
+
     return (
-        <IonPage>
-            <IonHeader>
-                <IonToolbar color={'success'}>
-                    <IonTitle>Free Code Camp</IonTitle>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent scrollY={false}>
-                <div className='ion-text-center ion-padding'>
-                    <img src={ FCC } alt='FCC Logo' width={'50%'}></img>
-                </div>
-               <IonCard>
-                    <IonCardContent>
-                        <form onSubmit={doLogin}>
-                            <IonInput fill='outline' labelPlacement='floating' label="Email" type="email" placeholder="email@domain.com"></IonInput>
-                            <IonInput className='ion-margin-top' fill='outline' labelPlacement='floating' label='Password' type='password'></IonInput>
-                            <IonButton className='ion-margin-top' type='submit' color={'tertiary'} expand='block'>
-                                Login
-                                <IonIcon icon={ logInOutline } slot="end"></IonIcon>
-                                </IonButton>
-                            <IonButton className='ion-margin-top' routerLink='/register' type='button' expand='block'>
-                                Create Account
-                                <IonIcon icon={ personCircleOutline } slot="end"></IonIcon>
-                                </IonButton>
-                        </form>
-                    </IonCardContent>
-               </IonCard>
-            </IonContent>
-            
-        </IonPage>
+        <>
+        { !introSeen ? (
+                <Intro onFinish={finishIntro}/>
+        ) : (
+            <IonPage>
+                <IonHeader>
+                    <IonToolbar color={'success'}>
+                        <IonTitle>Free Code Camp</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonContent scrollY={false}>
+                    <div className='ion-text-center ion-padding'>
+                        <img src={ FCC } alt='FCC Logo' width={'50%'}></img>
+                    </div>
+                <IonCard>
+                        <IonCardContent>
+                            <form onSubmit={doLogin}>
+                                <IonInput fill='outline' labelPlacement='floating' label="Email" type="email" placeholder="email@domain.com"></IonInput>
+                                <IonInput className='ion-margin-top' fill='outline' labelPlacement='floating' label='Password' type='password'></IonInput>
+                                <IonButton className='ion-margin-top' type='submit' color={'tertiary'} expand='block'>
+                                    Login
+                                    <IonIcon icon={ logInOutline } slot="end"></IonIcon>
+                                    </IonButton>
+                                <IonButton className='ion-margin-top' routerLink='/register' type='button' expand='block'>
+                                    Create Account
+                                    <IonIcon icon={ personCircleOutline } slot="end"></IonIcon>
+                                    </IonButton>
+                            </form>
+                        </IonCardContent>
+                </IonCard>
+                </IonContent>
+                
+            </IonPage>
+        )}
+        </>
     );
 };
 
