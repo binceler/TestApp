eed162e10a71db5cf4d631f47b0deb9252e7fae0
Büşra Inceler <busrainceler@gmail.com>
2025-01-04 14:55:04 +0300

    building a tab bar

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,5 +1,5 @@
-import { IonContent, IonHeader, IonItem, IonMenu, IonMenuToggle, IonPage, IonRouterOutlet, IonTitle, IonToolbar } from '@ionic/react';
-import { homeOutline, newspaperOutline } from 'ionicons/icons';
+import { IonButton, IonContent, IonHeader, IonIcon, IonItem, IonMenu, IonMenuToggle, IonPage, IonRouterOutlet, IonSplitPane, IonTitle, IonToolbar } from '@ionic/react';
+import { homeOutline, logOut, logOutOutline, newspaperOutline } from 'ionicons/icons';
 import React from 'react';
 import { Redirect, Route } from 'react-router';
 import List from './List';
@@ -8,34 +8,43 @@ import Settings from './Settings';
 const Menu: React.FC = () => {
     const paths = [
         {name: 'Home', url: '/app/list', icon: homeOutline},
-        {name: 'Settings', url: '/app/setting', icon: newspaperOutline},
+        {name: 'Settings', url: '/app/settings', icon: newspaperOutline},
     ];
 
     return (
         <IonPage>
-            <IonMenu contentId='main'>
-                <IonHeader>
-                    <IonToolbar color={"secondary"}>
-                        <IonTitle>Menu</IonTitle>
-                    </IonToolbar>
-                </IonHeader>
-                <IonContent>
-                    {paths.map((item, index) => (
-                        <IonMenuToggle key={index}>
-                            <IonItem routerLink={item.url} routerDirection="none">
-                                {item.name}
-                            </IonItem>
-                        </IonMenuToggle>
-                    ))}
-                </IonContent>
-            </IonMenu>
-            <IonRouterOutlet id='main'>
-                <Route exact path="/app/list" component={List} />
-                <Route path="/app/setting" component={Settings} />
-                <Route exact path="/app" component={Settings}>
-                    <Redirect to="app/list" />
-                </Route>
-            </IonRouterOutlet>
+            <IonSplitPane contentId='main'>
+                <IonMenu contentId='main'>
+                    <IonHeader>
+                        <IonToolbar color={"secondary"}>
+                            <IonTitle>Menu</IonTitle>
+                        </IonToolbar>
+                    </IonHeader>
+                    <IonContent>
+                        {paths.map((item, index) => (
+                            <IonMenuToggle key={index} autoHide={false}>
+                                <IonItem detail={false} routerLink={item.url} routerDirection="none">
+                                    <IonIcon slot='start' icon={item.icon} />
+                                    {item.name}
+                                </IonItem>
+                            </IonMenuToggle>
+                        ))}
+                        <IonMenuToggle autoHide={false}>
+                                <IonButton expand='full' routerLink="/" routerDirection="root">
+                                    <IonIcon slot='start' icon={logOutOutline} />
+                                    Logout
+                                </IonButton>
+                            </IonMenuToggle>
+                    </IonContent>
+                </IonMenu>
+                <IonRouterOutlet id='main'>
+                    <Route exact path="/app/list" component={List} />
+                    <Route path="/app/settings" component={Settings} />
+                    <Route exact path="/app">
+                        <Redirect to="/app/list" />
+                    </Route>
+                </IonRouterOutlet>
+            </IonSplitPane>
         </IonPage>
     );
 };
